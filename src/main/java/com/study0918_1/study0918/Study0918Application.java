3dc7c3e477db0f0006bf2982d67d1875bae84d46
장관영 <jkypch@gmail.com>
2023-09-18 11:19:16 +0900

    second commit

---
+++ src/main/java/com/study0918_1/study0918/Study0918Application.java
@@ -0,0 +1,13 @@
+package com.study0918_1.study0918;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0918Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study0918Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study0918_1/study0918/Study0918ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study0918_1.study0918;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study0918ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
